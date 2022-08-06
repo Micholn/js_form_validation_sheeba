2aff1c8728b97e11db1cae293b6b09ce150693f0
Micholn <mdiagbare@gmail.com>
2022-08-06 12:57:09 +0100

    added a few comments

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-const name = document.getElementById('name')
-const password = document.getElementById('password')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
+const name = document.getElementById("name"); //variable for name
+const password = document.getElementById("password"); //variable for password
+const form = document.getElementById("form"); 
+const errorElement = document.getElementById("error");
 
-form.addEventListener('submit', (e) => {
-    let messages = []
-    if(name.value === '' || name.value == null ) {
-       messages.push('Name is required') 
+/**to be able to catch these errors, you need to add event listeners to the form because by 
+ * default when you click submit it automatically refreshes the page  */
+
+//now we add the submit even listener
+//in order to cancel out our form submitting you add the (e)
+form.addEventListener("submit", (e) => {
+    let messages = []  /* creating a variable that is going to be equal to all of our error messsages*/
+    if (name.value === "" || name.value === null ) {
+        messages.push("name is required")
     }
 
-    if (password.value.length <= 6) {
-        messages.push('password must be longer than 6 characters')
+    if (password.value.length <= 6 ) {
+        messages.push("Password must be longer than six characters");
     }
 
-    if (password.value.length >= 20) {
-        messages.push('password must be less than 6 characters')
+    if (password.value.length >= 20 ) {
+        messages.push("password must be less than 20 characters")
     }
 
-    if(password.value === 'password'){
-        messages.push('password cannot be password')
+    if (password.value === "password") {
+        messages.push("password cannot be set to password");
     }
 
-    if (messages.length > 0) {
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
+    if (messages.length  > 0) {
+        e.preventDefault(); /**this called function is going to prevent our page from submitting, 
+        but we only want to do this if we have errors */ 
+        errorElement.innerText = messages.join(",")  /**here we are taking our error element and setting the 
+        innerText text here take our messafges and join each one of these by a comma so they are 
+        all separated by a comma */
     }
-   
-})
+ 
+});
